c15337fbf45db10e4bafe09108bb5333e7921a0c
jd <duverseaujohnny21@gmail.com>
2020-10-23 18:50:19 -0400

    add all files .cshtml and codes

---
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetAndSavory.Models
+{
+  public class SweetAndSavoryContext : DbContext
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public SweetAndSavoryContext(DbContextOptions options) : base(options) { }
+  }
+}
